75947b36dd29056e36bdbc332afba0d606a66bcd
unknown <zoukeqiang@PC-201508081135.jk.com>
2016-08-01 15:35:57 +0800

    初始提交

---
+++ src/test/java/testweb/TestResult.java
@@ -0,0 +1,12 @@
+package testweb;
+
+import org.junit.Test;
+
+public class TestResult {
+	@Test
+	public void jisuan(){
+		String ss = "asdfgjkl;qwertuiopyzzxcvbmm";
+		int longSs = ss.length();
+		System.out.println(longSs);
+	}
+}
