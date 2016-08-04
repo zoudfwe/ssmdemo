db74b90ce396962d3ab33a5297986e454d415271
zoudfwe <1054314004@qq.com>
2016-08-04 14:09:18 +0800

    ssm  +  mybatis-generator  mysql自动生成

---
+++ mybatis-generator/createTable.sql
@@ -0,0 +1,17 @@
+create table muser(
+id varchar2(36) primary key,
+name varchar2(36),
+age number(8),
+address varchar2(36)
+);
+
+CREATE TABLE `ACT_ID_USER` (
+  `ID_` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `REV_` int(11) DEFAULT NULL,
+  `FIRST_` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `LAST_` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `EMAIL_` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `PWD_` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `PICTURE_ID_` varchar(64) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`ID_`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
---
+++ src/main/java/rml/dao/ActIdUserMapper.java
@@ -0,0 +1,17 @@
+package rml.dao;
+
+import rml.model.ActIdUser;
+
+public interface ActIdUserMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(ActIdUser record);
+
+    int insertSelective(ActIdUser record);
+
+    ActIdUser selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(ActIdUser record);
+
+    int updateByPrimaryKey(ActIdUser record);
+}
